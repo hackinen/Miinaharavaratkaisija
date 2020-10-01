49ee057040f797a20261d7ffac79c6af428a12c7
Iira <iira.hakkinen@helsinki.fi>
2020-10-01 20:16:41 +0300

    tietorakenteen aloitus

--- minesweeper-master/src/main/java/minesweeper/structures/ListOfPairs.java
+++ minesweeper-master/src/main/java/minesweeper/structures/ListOfPairs.java
@@ -5,10 +5,25 @@
  */
 package minesweeper.structures;
 
+import minesweeper.model.*;
+
 /**
- *
+ * A datastructure to replace ArrayList
  * @author hiira
  */
 public class ListOfPairs {
+    private Pair[] list;
+    private int size;
     
+    public ListOfPairs(int size) {
+        this.size = size;
+        this.list = new Pair[size];
+    }
+    
+    /**
+     * Doubles the size of the list
+     */
+    public void increaseSizeOfList() {
+        
+    }
 }
