c7d596d039491505eb166b391c3b2190c09b98f3
Iira <iira.hakkinen@helsinki.fi>
2020-10-02 17:04:54 +0300

    tietorakenne

--- minesweeper-master/src/main/java/minesweeper/structures/ListOfPairs.java
+++ minesweeper-master/src/main/java/minesweeper/structures/ListOfPairs.java
@@ -13,17 +13,102 @@ import minesweeper.model.*;
  */
 public class ListOfPairs {
     private Pair[] list;
-    private int size;
+    private int size;       // absolute size of the list: empty cells too
+    private int length;     // only cells that have a value
     
-    public ListOfPairs(int size) {
-        this.size = size;
+    
+    public ListOfPairs() {
+        this.size = 1;
+        this.length = 0;
         this.list = new Pair[size];
     }
+    
     
     /**
      * Doubles the size of the list
      */
     public void increaseSizeOfList() {
+        Pair[] newList = new Pair[this.size * 2];
         
+        for (int i = 0; i < this.length; i++) {
+            newList[i] = this.list[i];
+        }
+        this.list = newList;
+        this.size = this.size * 2;
+    }
+    
+    
+    /**
+     * Checks if the given list is empty or not
+     * @return true if empty, false if not
+     */
+    public boolean isEmpty() {
+        if (this.length == 0) {
+            return true;
+        }
+        
+        return false;
+    }
+    
+    
+    /**
+     * Returns the size of the list (the number of Pairs)
+     * @return size
+     */
+    public int size() {
+        return this.length;
+    }
+    
+    
+    /**
+     * Adds a new pair to the end of the list
+     * @param pair 
+     */
+    public void add(Pair pair) {
+        
+        if (this.length == this.size) {
+            increaseSizeOfList();
+        }
+        
+        this.list[this.length] = pair;
+        this.length++;
+    }
+    
+    
+    /**
+     * returns and removes the value in list[i]
+     * @param i index
+     * @return Pair in index i
+     */
+    public Pair remove(int i) {
+        
+        if (i >= this.length) {
+            return null;
+        }
+        
+        Pair pair = this.list[i];
+        Pair[] newList = new Pair[this.size];
+        int offset = 0;
+        
+        for (int x = 0; x < this.length; x++) {
+            if (x == i) {
+                offset++;
+                continue;
+            }
+            
+            newList[x-offset] = this.list[x];
+        }
+        
+        return pair;
+    }
+    
+    
+    /**
+     * returns the value in list[i]
+     * @param i index
+     * @return Pair in index i
+     */
+    public Pair get(int i) {
+        return this.list[i];
     }
 }
